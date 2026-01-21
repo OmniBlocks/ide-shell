260695923865c91c1dfcf122f4e1dbdba9bab555
8to16 <ampelectrecuted@gmail.com>
2026-01-21 22:18:52 +0000

    compression

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { compression } from 'vite-plugin-compression2'
+import { ZstdCodec } from 'zstd-codec'
+import { Buffer } from 'buffer';
 
-// https://vite.dev/config/
+const zstdCompress = async (buf) => {
+  return new Promise((resolve) => {
+    ZstdCodec.run((zstd) => {
+      const simple = new zstd.Simple();
+      const compressed = simple.compress(buf, 3);
+      resolve(Buffer.from(compressed));
+    });
+  });
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'brotliCompress',
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+    compression({
+      algorithm: zstdCompress,
+      filename: '[path][base].zst',
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+  ],
   base: './',
 })
